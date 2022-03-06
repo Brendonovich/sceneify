8540598ae75e56cf2a90e2e35cdbfbdf77db85af
Brendan Allan <brendonovich@outlook.com>
2022-03-06 17:30:35 +0800

    feat(sources): improve media source

--- packages/sources/src/Media.ts
+++ packages/sources/src/Media.ts
@@ -1,14 +1,85 @@
-import { Input, SourceFilters, CustomInputArgs } from "@sceneify/core";
+import {
+  Input,
+  SourceFilters,
+  CustomInputArgs,
+  OBSEventTypes,
+} from "@sceneify/core";
+import { EventEmitter } from "eventemitter3";
+
+export enum ColorRange {
+  Default = 0,
+  Partial = 1,
+  Full = 2,
+}
 
 export type MediaSourceSettings = {
   local_file: string;
+  is_local_file: boolean;
+  looping: boolean;
+  restart_on_activate: boolean;
+  buffering_mb: number;
+  input: string;
+  input_format: string;
+  reconnect_delay_sec: number;
   hw_decode: boolean;
+  clear_on_media_end: boolean;
+  close_when_inactive: boolean;
+  speed_percent: number;
+  color_range: ColorRange;
+  linear_alpha: boolean;
+  seekable: boolean;
 };
+
+export interface MediaSourceEvents {
+  PlaybackStarted: void;
+  PlaybackEnded: void;
+}
+
 export class MediaSource<Filters extends SourceFilters = {}> extends Input<
   MediaSourceSettings,
   Filters
 > {
+  private emitter = new EventEmitter();
+
+  private emit(event: keyof MediaSourceEvents) {
+    this.emitter.emit(event);
+  }
+
+  startedListener = (args: OBSEventTypes["MediaInputPlaybackStarted"]) => {
+    if (this.name === args.inputName) {
+      this.emit("PlaybackStarted");
+    }
+  };
+
+  endedListener = (args: OBSEventTypes["MediaInputPlaybackEnded"]) => {
+    if (this.name === args.inputName) {
+      this.emit("PlaybackEnded");
+    }
+  };
+
   constructor(args: CustomInputArgs<MediaSourceSettings, Filters>) {
     super({ ...args, kind: "ffmpeg_source" });
+
+    this.obs.on("MediaInputPlaybackStarted", this.startedListener);
+    this.obs.on("MediaInputPlaybackEnded", this.endedListener);
+  }
+
+  override async remove() {
+    await super.remove();
+
+    this.obs.off("MediaInputPlaybackStarted", this.startedListener);
+    this.obs.off("MediaInputPlaybackEnded", this.endedListener);
+  }
+
+  on(event: keyof MediaSourceEvents, fn: () => void) {
+    return this.emitter.on(event, fn);
+  }
+
+  off(event: keyof MediaSourceEvents, fn: () => void) {
+    return this.emitter.off(event, fn);
+  }
+
+  once(event: keyof MediaSourceEvents, fn: () => void) {
+    return this.emitter.once(event, fn);
   }
 }
