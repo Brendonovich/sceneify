40bc26a0ae9c3b51dd2272f01eb96638121a98e0
Brendan Allan <brendonovich@outlook.com>
2022-09-13 01:41:00 +0800

    plz

--- packages/core/src/Input.ts
+++ packages/core/src/Input.ts
@@ -156,7 +156,7 @@ export class Input<
 
     if (this.exists) {
       this.obs.inputs.set(this.name, this);
-      await this.setSettings(this.creationArgs.settings ?? {} as any, false);
+      await this.setSettings(this.creationArgs.settings ?? ({} as any), false);
     }
   }
 
